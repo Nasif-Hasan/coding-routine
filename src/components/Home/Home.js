dc0dbd2bca5d2882cdc2358d449b0d23069411fa
Nasif-Hasan <nasifhasan35@gmail.com>
2022-09-29 20:38:14 +0600

    cart-part

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,28 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import Programs from '../Programs/Programs';
 import './Home.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
-import BreakBox from '../BreakBox/BreakBox';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+
+
+import SideCart from '../SideCart/SideCart';
 
 const Home = () => {
 
     const [programs, setPrograms] = useState([])
-    const [space, setSpace] = useState([])
-
+    
     const [cart, setCart] = useState([])
 
-    const notify = () => toast("Wow so easy!");
     
     useEffect(() => {
         fetch('data.json')
             .then(res => res.json())
             .then(data => setPrograms(data))
     }, [])
-
-    
 
 
     return (
@@ -36,55 +30,10 @@ const Home = () => {
                 ></Programs>
             </div>
             <div className='side-cart'>
-                <div className='profile'>
-                    <img className='profile-img' src="https://cdn.dribbble.com/users/1174720/screenshots/15718185/media/b54ee56400b00386f558b6a6f465d5b0.png?compress=1&resize=320x240&vertical=top" alt="" />
-                    <div>
-                        <h3>Nasif Hasan</h3>
-                        <p><FontAwesomeIcon className='location' icon={faLocationDot}></FontAwesomeIcon> Dhaka</p>
-                    </div>
-                </div>
-                <div className='info-box'>
-                    <div>
-                        <h2>56</h2>
-                        <p>Weight</p>
-                    </div>
-                    <div>
-                        <h2>5'6"</h2>
-                        <p>Height</p>
-                    </div>
-                    <div>
-                        <h2>20</h2>
-                        <p>Age</p>
-                    </div>
-
-                </div>
-
-                <div>
-                    <BreakBox
-                    space={space}
-                    setSpace={setSpace}
-                    ></BreakBox>
-                </div>
-
-                <div>
-                    <h2>Total Learning Time</h2>
-                    
-                    <div className='info-box'>
-                        <h4>Total Time: {cart.map((p) => (p.time))} m</h4>
-                    </div>
-
-                    <div className='info-box'>
-                        <h4>Break Time: 00 Minutes</h4>
-                    </div>
-
-                </div>
-
-                <div>
-                    <button onClick={notify} className='btn-activity'>
-                        <h3>Coding Activity</h3>
-                    </button>
-                    <ToastContainer />
-                </div>
+                <SideCart
+                    cart={cart}
+                    setCart={setCart}
+                ></SideCart>
 
             </div>
         </div>
---
+++ src/components/SideCart/SideCart.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import './SideCart.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import BreakBox from '../BreakBox/BreakBox';
+
+const SideCart = ({cart, setCart}) => {
+
+    const [space, setSpace] = useState([])
+    const notify = () => toast("Wow so easy!");
+
+    return (
+        <div>
+            <div className='profile'>
+                    <img className='profile-img' src="https://cdn.dribbble.com/users/1174720/screenshots/15718185/media/b54ee56400b00386f558b6a6f465d5b0.png?compress=1&resize=320x240&vertical=top" alt="" />
+                    <div>
+                        <h3>Nasif Hasan</h3>
+                        <p><FontAwesomeIcon className='location' icon={faLocationDot}></FontAwesomeIcon> Dhaka</p>
+                    </div>
+                </div>
+                <div className='info-box'>
+                    <div>
+                        <h2>56</h2>
+                        <p>Weight</p>
+                    </div>
+                    <div>
+                        <h2>5'6"</h2>
+                        <p>Height</p>
+                    </div>
+                    <div>
+                        <h2>20</h2>
+                        <p>Age</p>
+                    </div>
+
+                </div>
+
+                <div>
+                    <BreakBox
+                    space={space}
+                    setSpace={setSpace}
+                    ></BreakBox>
+                </div>
+
+                <div>
+                    <h2>Total Learning Time</h2>
+                    
+                    <div className='info-box'>
+                        <h4>Total Time: {cart.map((p) => (p.time))} m</h4>
+                    </div>
+
+                    <div className='info-box'>
+                        <h4>Break Time: 00 Minutes</h4>
+                    </div>
+
+                </div>
+
+                <div>
+                    <button onClick={notify} className='btn-activity'>
+                        <h3>Coding Activity</h3>
+                    </button>
+                    <ToastContainer />
+                </div>
+        </div>
+    );
+};
+
+export default SideCart;
